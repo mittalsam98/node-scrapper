ff3cacbdebf67479d896307656014387b403c9fe
Sachin <mittalsam98@gmail.com>
2020-03-19 23:44:04 +0530

    added instagram followers count

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-import {getHtml} from './scrapper.js';
+import {getHtml,getInstagramFollowers} from './scrapper.js';
 
- console.log(getHtml());
+
+async function go(){
+ const html  = await getHtml('https://www.instagram.com/decent_sachin.mittal/') ;
+    
+ const instagramCount=await getInstagramFollowers(html);
+console.log('You have '+instagramCount+' instagram follwers');
+}
+
+go(); 
--- scrapper.js
+++ scrapper.js
@@ -1,4 +1,18 @@
+
+import axios from 'axios'
+import cheerio from 'cheerio'
+
 export async function getHtml(url){
-    const html='cool';
+    const {data:html}=await axios.get(url);
     return html;
+}
+
+
+
+export async function getInstagramFollowers(html){
+    const $=cheerio.load(html);
+    const dataString=$('script[type="application/ld+json"]').html();
+    const pageObject=JSON.parse(dataString);
+    // return span.data('count'); 
+    return (parseInt(pageObject.mainEntityofPage.interactionStatistic.userInteractionCount))
 }
